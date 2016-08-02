3f2fe565ac1bf48908c7e649fbfc7dcfe6ff230c
David Warring <david.warring@gmail.com>
2016-08-03 10:19:14 +1200

    add text valign.
    Seems there's no easy, or standard way to do this in pure CSS.
    Implement with HTML support.

--- lib/PDF/Style/Viewport.pm
+++ lib/PDF/Style/Viewport.pm
@@ -12,7 +12,7 @@ class PDF::Style::Viewport {
     has $.width = 595pt;
     has $.height = 842pt;
 
-    method text( Str $text, CSS::Declarations :$css!) {
+    method text( Str $text, CSS::Declarations :$css!, Str :$valign is copy) {
         my $position = $css.position;
         die "sorry can only handle 'position: absolute' at the moment"
             unless $position ~~ 'absolute';
@@ -21,45 +21,44 @@ class PDF::Style::Viewport {
         die "sorry cannot handle right positioning yet"
             unless $css.right eq 'auto';
 
-        my $left = pt($css.left) // 0pt;
-        my $top = pt($css.top) // 0pt;
         my $family = $css.font-family // 'arial';
         my $weight = $css.font-weight // 'normal';
         my $font-style = $css.font-style // 'normal';
         # todo: derive default from the canvas
         my $font-size = pt($css.font-size) // 12pt;
+        my $font = PDF::Content::Util::Font::core-font( :$family, :$weight, :style($font-style) );
+        my $em = $font-size;
+        my $ex = $font-size * $_ / 1000
+            with $font.XHeight;
+
+        my $left = pt($css.left, :$em, :$ex) // 0pt;
+        my $css-top =pt($css.top, :$em, :$ex) // 0pt;
         my Numeric $width = pt($css.width) // self.width;
 
-        my $height = pt($css.height) // self.height - $top;
+        my $height = pt($css.height) // self.height - $css-top;
         my $line-height = pt($css.line-height) // $font-size * 1.2;
 
         # todo - rtfm on auto widths & page boundarys
-        warn "can't cope yet: {:$width} {:$height} {:$top} {:$left}"
-            unless $width > 0 && $height > 0 && $left >= 0 && $left < self.width && $top >= 0 && $top < self.height;
-
-        my $font = PDF::Content::Util::Font::core-font( :$family, :$weight, :style($font-style) );
+        warn "can't cope yet: {:$width} {:$height} {:$css-top} {:$left}"
+            unless $width > 0 && $height > 0 && $left >= 0 && $left < self.width && $css-top >= 0 && $css-top < self.height;
 
         my $kern = $css.font-kerning
             && ( $css.font-kerning eq 'normal'
                  || ($css.font-kerning eq 'auto' && $font-size <= 32));
 
-        my $align = $css.text-align eq 'left'|'right'|'center'|'justify'
+        my $align = $css.text-align && $css.text-align eq 'left'|'right'|'center'|'justify'
             ?? $css.text-align
             !! 'left';
-        my $text-block = PDF::Content::Text::Block.new( :$text, :$font, :$kern, :$font-size, :$line-height, :$width, :$height, :$align, :valign<top> );
 
-        my $x = $left;
-        $x += $text-block.width * ($align eq 'right' ?? 1 !! $align eq 'center' ?? 0.5 !! 0);
-        my $y = pt($.height) - $top;
+        $valign //= 'top';
+        my $text-block = PDF::Content::Text::Block.new( :$text, :$font, :$kern, :$font-size, :$line-height, :$width, :$height, :$align, :$valign );
 
-        my $em = $font-size;
-        my $ex = $font-size * $_ / 1000
-            with $font.XHeight;
-
+        # convert back to pdf coordinates
+        my $top = pt($.height) - $css-top;
         $width  = pt($css.width,  :$em, :$ex) // $text-block.actual-width;
         $height = pt($css.height, :$em, :$ex) // $text-block.actual-height;
-        my $box = PDF::Style::Box.new: :$css, :$left, :top($y), :$width, :$height, :$em, :$ex;
+        my $box = PDF::Style::Box.new: :$css, :$left, :$top, :$width, :$height, :$em, :$ex;
 
-        $text-block, $box, $x, $y;
+        $text-block, $box;
     }
 }
--- t/00basic.t
+++ t/00basic.t
@@ -26,26 +26,69 @@ for <left center right justify> -> $alignment {
         ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
         --ENOUGH!!--
 
-    note "% **** $alignment *** ";
+    note "% **** align $alignment *** ";
     $css.text-align = :keyw($alignment);
-    my @fw = 'bold', 'normal';    
+    $css.font-weight = :keyw<bold>;
         
     for $header, $body {
-        $css.font-weight = :keyw(@fw.shift);
         my Str $style = $css.write;
         my Str $text = $_;
         $text ~= ' ' ~ $style if $css.font-weight eq 'normal';
-        my ($text-block, $box, $left, $top) = $vp.text( $text, :$css );
+        my ($text-block, $box) = $vp.text( $text, :$css );
         @html.push: sprintf '<div style="%s">%s</div>', $style, $text;
         $page.graphics: {
             $box.style($_);
             $page.text: {
-                .print($text-block, :position[$left, $top]);
+                .paragraph($text-block, :position[$box.left, $box.top]);
             }
         }
         $css.top += 15pt;
+        $css.font-weight = :keyw<normal>;
     }
-    $css.top += 90pt;
+    $css.top += 100pt;
+}
+
+$css.delete("text-align");
+$css.vertical-align = :keyw<left>;
+$css.top = 30pt;
+$css.left = 350pt;
+$css.width = 220pt;
+
+for <top center bottom> -> $valign {
+    my $header = [~] '*** VALIGN:', $valign, ' ***', "\n";
+    my $body = q:to"--ENOUGH!!--".lines.join: ' ';
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        --ENOUGH!!--
+
+    note "% **** valign $valign *** ";
+    $css.delete("height");
+    $css.font-weight = :keyw<bold>;
+    my Str $style = $css.write;
+        
+    my ($text-block, $box) = $vp.text( $header, :$css );
+    @html.push: sprintf '<div style="%s">%s</div>', $style, $header;
+    $page.graphics: {
+        $box.style($_);
+        $page.text: {
+            .print($text-block, :position[$box.left, $box.top]);
+        }
+    }
+
+    $css.top += 15pt;
+    $css.font-weight = :keyw<normal>;
+    $css.height = 130pt;
+    $style = $css.write;
+    my $text = $body ~ $style;
+    ($text-block, $box) = $vp.text( $text, :$css, :$valign );
+    @html.push: sprintf '<div style="%s"><div style="position:relative; top:%dpt">%s</div></div>', $style, $text-block.top-offset, $text;
+    $page.graphics: {
+        $box.style($_);
+        $page.text: {
+            .paragraph($text-block, :position[$box.left, $box.top]);
+        }
+    }
+
+    $css.top += 165pt;
 
 }
 
