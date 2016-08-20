079c2e15d4ef5ea13d90fea6e5d21999daada803
David Warring <david.warring@gmail.com>
2016-08-21 09:51:02 +1200

    persistant :em & :ex values in viewport

--- lib/PDF/Style/Viewport.pm
+++ lib/PDF/Style/Viewport.pm
@@ -9,8 +9,14 @@ class PDF::Style::Viewport {
     use PDF::Style :pt;
     use PDF::Style::Box;
 
-    has $.width = 595pt;
-    has $.height = 842pt;
+    has Numeric $.width = 595pt;
+    has Numeric $.height = 842pt;
+    has Numeric $.em = 12pt;
+    has Numeric $.ex = 9pt;
+
+    method pt($v) {
+        pt($v, :$!em, :$!ex);
+    }
 
     method text( Str $text, CSS::Declarations :$css!, Str :$valign is copy) {
 
@@ -23,18 +29,18 @@ class PDF::Style::Viewport {
         my $weight = $css.font-weight // 'normal';
         my $font-style = $css.font-style // 'normal';
         # todo: derive default from the canvas
-        my Numeric $font-size = { :medium(12pt), :large(16pt), :small(9pt) }{$css.font-size} // pt($css.font-size) // 12pt;
+        my Numeric $font-size = { :medium(12pt), :large(16pt), :small(9pt) }{$css.font-size} // self.pt($css.font-size) // 12pt;
         my $font = PDF::Content::Util::Font::core-font( :$family, :$weight, :style($font-style) );
-        my $em = $font-size;
-        my $ex = $font-size * $_ / 1000
+        $!em = $font-size;
+        $!ex = $font-size * $_ / 1000
             with $font.XHeight;
 
-        my $left = pt($css.left, :$em, :$ex) // 0pt;
-        my $css-top = pt($css.top, :$em, :$ex) // 0pt;
-        my Numeric $width = pt($css.width) // self.width;
+        my $left = self.pt($css.left) // 0pt;
+        my $css-top = self.pt($css.top) // 0pt;
+        my Numeric $width = self.pt($css.width) // self.width;
 
-        my $height = pt($css.height) // self.height - $css-top;
-        my $line-height = pt($css.line-height) // $font-size * 1.2;
+        my $height = self.pt($css.height) // self.height - $css-top;
+        my $line-height = self.pt($css.line-height) // $font-size * 1.2;
 
         # todo - rtfm on auto widths & page boundarys
         warn "can't cope yet: {:$width} {:$height} {:$css-top} {:$left}"
@@ -52,10 +58,10 @@ class PDF::Style::Viewport {
         my $text-block = PDF::Content::Text::Block.new( :$text, :$font, :$kern, :$font-size, :$line-height, :$width, :$height, :$align, :$valign );
 
         # convert back to pdf coordinates
-        my $top = pt($.height) - $css-top;
-        $width  = pt($css.width,  :$em, :$ex) // $text-block.actual-width;
-        $height = pt($css.height, :$em, :$ex) // $text-block.actual-height;
-        PDF::Style::Box.new: :$css, :$left, :$top, :$width, :$height, :$em, :$ex, :content($text-block);
+        my $top = self.pt($.height) - $css-top;
+        $width  = self.pt($css.width) // $text-block.actual-width;
+        $height = self.pt($css.height) // $text-block.actual-height;
+        PDF::Style::Box.new: :$css, :$left, :$top, :$width, :$height, :$!em, :$!ex, :content($text-block);
     }
 
 }
