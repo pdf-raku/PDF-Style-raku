323cf4cf57dbbc0916effe7fad358b013c2739b5
David Warring <david.warring@gmail.com>
2017-03-17 10:11:50 +1300

    wip on sizing/rendering of viewport

--- lib/PDF/Style/Viewport.pm
+++ lib/PDF/Style/Viewport.pm
@@ -6,6 +6,45 @@ use CSS::Declarations::Units;
 class PDF::Style::Viewport
     is PDF::Style::Box {
 
+    method !setup-size {
+        use PDF::Content::Page :PageSizes;
+        my @length;
+        my $orientation = 'portrait';
+
+        for self.css.size.list {
+            when Numeric {
+                @length.push: $_;
+            }
+            when 'portrait' | 'landscape' {
+                $orientation = $_;
+            }
+            when 'auto' {
+            }
+            when PageSizes.enums{.uc}:exists {
+                my Array $size = PageSizes.enums{.uc};
+                @length = $size[2], $size[3];
+            }
+            default {
+                warn "unhandled viewport 'size' {.perl}";
+            }
+        }
+
+        if @length {
+            @length[1] //= @length[0];
+            (self.width, self.height) = @length;
+        }
+
+        (self.height, self.width) = (self.width, self.height)
+            if $orientation eq 'landscape';
+    }
+
+    method TWEAK {
+        self!setup-size;
+        # todo: see https://www.w3.org/TR/css3-page/
+        # @top-left-corner etc
+        # page-break-before, page-break-after etc
+    }
+
     method !setup-page($page) {
         $page.media-box = [0, 0, self.width, self.height ];
         # draw borders + background image
--- t/viewport.t
+++ t/viewport.t
@@ -2,15 +2,30 @@ use v6;
 use Test;
 use PDF::Style::Viewport;
 use CSS::Declarations;
+use PDF::Lite;
 
 my $bg-image = PDF::Content::Image.open("t/images/semitransparent.gif");
 my $css = CSS::Declarations.new: :style("size: a5; margin-left: .5cm; background-image: url($bg-image); background-color: blue; opacity: .3; border: 1px solid red");
-warn $css;
+warn ~$css;
 
-todo "write general view point styling tests", 3;
 my $vp = PDF::Style::Viewport.new: :$css;
-is $vp.width, 420;
-is $vp.height, 595;
-flunk 'general styling tests';
+is $vp.width, 420, 'size - standard';
+is $vp.height, 595, 'size - standard';
+
+my $pdf = PDF::Lite.new;
+$vp.add-page($pdf);
+$pdf.save-as: "t/viewport.pdf";
+
+my $vp2 = PDF::Style::Viewport.new: :style("size: 200pt 300pt");
+is $vp2.width, 200, 'size - width + height';
+is $vp2.height, 300, 'size - width + height';
+
+$vp2 = PDF::Style::Viewport.new: :style("size: 300px");
+is $vp2.width, 300,'size - width only';
+is $vp2.height, 300, 'size - width only';
+
+$vp2 = PDF::Style::Viewport.new: :style("size: a5 landscape");
+is $vp2.width, 595,'size - width only';
+is $vp2.height, 420, 'size - width only';
 
 done-testing;
